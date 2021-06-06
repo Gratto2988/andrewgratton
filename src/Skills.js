c9db88291dc119181974917182e60ce0efe7d724
Andrew Gratton <agratton2988@googlemail.com>
2021-06-06 15:25:38 +0100

    updated footer

--- src/Skills.js
+++ src/Skills.js
@@ -5,32 +5,46 @@ import Star from './Star';
 const Skills = () => {        
     return(
         <div >
+        <div className="navPad"></div>
         <h2 className="padStyle "><center>My Skills</center></h2>
 
         <div className="row">
             
             <div className="col-md-2"></div>
 
-            <div className="col-md-4 details">
-                <ul>
-                    <li>HTML 5</li>
-                    <li>CSS 3</li>
-                    <li>JavaScript</li>
-                    <li>PHP</li>
-                    <li>MySQL</li>
-                    <li>React</li>
-                </ul>
-            </div>
+            <div className="col-md-8 details">
+            <div className="bc padStyle details">
+                <center><p>These stars are based from a personal viewpoint of my skill </p> </center>
+                <table className="skillTree">
+                    <tbody>
+                        <tr>
+                            <td>HTML 5</td>
+                            <td><Star star="4"/></td>
+                        </tr>
+                        <tr>
+                            <td>CSS 3</td>
+                            <td><Star star="4"/></td>
+                        </tr>
+                        <tr>
+                            <td>JavaScript</td>
+                            <td><Star star="4"/></td>
+                        </tr>
+                        <tr>
+                            <td>PHP</td>
+                            <td><Star star="3"/></td>
+                        </tr>
+                        <tr>
+                            <td>MySQL</td>
+                            <td><Star star="4"/></td>
+                        </tr>
+                        <tr>
+                            <td>React</td>
+                            <td><Star star="3"/></td>
+                        </tr>
+                    </tbody>
 
-            <div className="col-md-4 details">
-                <ul className="ratings">
-                    <Star star="4"/>
-                    <Star star="4"/>
-                    <Star star="4"/>
-                    <Star star="3"/>
-                    <Star star="3"/>
-                    <Star star="3"/>
-                </ul>
+                </table>
+            </div>
             </div>
 
             <div className="col-md-2"></div>
@@ -40,3 +54,4 @@ const Skills = () => {
 }
 
 export default Skills;
+
